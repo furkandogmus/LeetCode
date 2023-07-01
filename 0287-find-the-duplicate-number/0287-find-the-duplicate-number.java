39387e0f98fb83a2191a63bec096ae2846213909
Furkan Doğmuş <69652349+furkandogmus@users.noreply.github.com>
2023-07-01 07:28:43 +0300

    Time: 2 ms (98.37%), Space: 57.5 MB (34.89%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
@@ -1,12 +1,12 @@
 class Solution {
     public int findDuplicate(int[] nums) {
-            for(int num:nums){
-            int index = Math.abs(num);
-            if(nums[index]<0){
-                return index;
+        int[] temp = new int[nums.length];
+        for(int num:nums){
+            if(temp[num]==1){
+                return num;
             }
-            nums[index] *=-1;
-        }
+            temp[num] = 1;
+    }
     return 0;
     }
 }
