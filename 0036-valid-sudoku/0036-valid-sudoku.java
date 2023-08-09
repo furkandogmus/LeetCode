bd12eb92da86d76cd96da1b676f17d6b25d7c2c5
Furkan Doğmuş <69652349+furkandogmus@users.noreply.github.com>
2023-08-10 00:49:48 +0300

    Time: 5 ms (42.45%), Space: 43.2 MB (75.67%) - LeetHub

---
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -0,0 +1,57 @@
+class Solution {
+    public boolean isValidSudoku(char[][] board) {
+        return checkRows(board) && checkCols(board) && check3x3Squares(board);
+    }
+    
+    public boolean checkRows(char[][] board) {
+        int rows = 9;
+        int cols = 9;
+        for (int i = 0; i < rows; i++) {
+            HashSet<Character> hs = new HashSet<>();
+            for (int j = 0; j < cols; j++) {
+                if (!hs.contains(board[i][j]) || board[i][j] == '.') {
+                    hs.add(board[i][j]);
+                } else {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+    
+    public boolean checkCols(char[][] board) {
+        int rows = 9;
+        int cols = 9;
+        for (int i = 0; i < rows; i++) {
+            HashSet<Character> hs = new HashSet<>();
+            for (int j = 0; j < cols; j++) {
+                if (!hs.contains(board[j][i]) || board[j][i] == '.') {
+                    hs.add(board[j][i]);
+                } else {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+    
+    public boolean check3x3Squares(char[][] board) {
+        int rows = 9;
+        int cols = 9;
+        for (int i = 0; i < rows; i += 3) {
+            for (int j = 0; j < cols; j += 3) {
+                HashSet<Character> hs = new HashSet<>();
+                for (int k = i; k < i + 3; k++) {
+                    for (int p = j; p < j + 3; p++) {
+                        if (!hs.contains(board[k][p]) || board[k][p] == '.') {
+                            hs.add(board[k][p]);
+                        } else {
+                            return false;
+                        }
+                    }
+                }
+            }
+        }
+        return true;
+    }
+}
