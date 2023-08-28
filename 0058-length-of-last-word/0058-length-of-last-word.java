983642651b6d91097a2840c5a040d7d28becf118
Furkan Doğmuş <69652349+furkandogmus@users.noreply.github.com>
2023-08-29 00:21:30 +0300

    Time: 3 ms (8.96%), Space: 40.8 MB (39.74%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.java
+++ 0058-length-of-last-word/0058-length-of-last-word.java
@@ -1,22 +1,6 @@
 class Solution {
     public int lengthOfLastWord(String s) {
-        int last = 0;
-        int result = 0;
-        for(int i=0;i<s.length();i++){
-            if(s.charAt(i)!= ' '){
-                last++;
-            }else{
-                if(last!=0){
-                result = last;
-                }
-                last= 0;
-            }
-            
-        }
-        if(last!=0){
-                result = last;
-        }
-        
-        return result;
+        String[] strs = s.split("\\s+");
+        return strs[strs.length-1].length();
     }
 }
