494684e49bfbd640786c4dbc624d9c67cfec6582
Furkan Doğmuş <69652349+furkandogmus@users.noreply.github.com>
2023-08-31 00:24:15 +0300

    Time: 22 ms (31.56%), Space: 44.7 MB (43.62%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.java
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -10,7 +10,7 @@ class Solution {
         for(int i=n-2;i>=0;i--){
             if(nums[i]+i>=n-1){minPaths[i] = 1;}
             else{
-                int min = Integer.MAX_VALUE;
+                int min = 10000;
                 for(int x=i;x<=nums[i]+i;x++){
            
                     min = Math.min(min,minPaths[x]+1);
