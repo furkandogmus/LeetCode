ac612e27cb851b532d099ff2afe04247fa850417
Furkan Doğmuş <69652349+furkandogmus@users.noreply.github.com>
2023-08-31 00:24:50 +0300

    Time: 24 ms (31.18%), Space: 44.3 MB (86.30%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.java
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -5,12 +5,12 @@ class Solution {
             return 0;
         }
         int[] minPaths = new int[n];
-        Arrays.fill(minPaths,10000);
+        Arrays.fill(minPaths,Integer.MAX_VALUE-5);
         minPaths[n-1] = 0;
         for(int i=n-2;i>=0;i--){
             if(nums[i]+i>=n-1){minPaths[i] = 1;}
             else{
-                int min = 10000;
+                int min = Integer.MAX_VALUE-5;
                 for(int x=i;x<=nums[i]+i;x++){
            
                     min = Math.min(min,minPaths[x]+1);
