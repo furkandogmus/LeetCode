a76ff362f1ed62a276e34e8638562ba4eceb4d4e
Furkan Doğmuş <69652349+furkandogmus@users.noreply.github.com>
2023-09-18 12:26:09 +0300

    Time: 16 ms (44.73%), Space: 43.9 MB (38.55%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,7 +1,6 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        s = s.toLowerCase();
-        s = s.replaceAll("[^a-z0-9]","");
+        s = s.toLowerCase().replaceAll("[^a-z0-9]","");
         int l=0,r=s.length()-1;
         while(l<r){
             if(s.charAt(l)==s.charAt(r)){
