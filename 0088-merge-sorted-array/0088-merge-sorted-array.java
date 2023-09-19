cb07d6ac6bdcbfc1fdf56554075caa65dae94168
Furkan Doğmuş <69652349+furkandogmus@users.noreply.github.com>
2023-09-19 21:35:44 +0300

    Time: 0 ms (100.00%), Space: 41.1 MB (83.23%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -1,10 +1,5 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        if(n==0) return;
-        if(m==0){
-            if (n >= 0) System.arraycopy(nums2, 0, nums1, 0, n);
-            return;
-        }
 
         int i = m-1,j=n-1;
         while(i>=0 || j>=0){
