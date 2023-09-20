a7a789ef67078b15b491169e549de71b6f04ad21
Furkan Doğmuş <69652349+furkandogmus@users.noreply.github.com>
2023-09-20 13:50:20 +0300

    Time: 0 ms (100.00%), Space: 38.9 MB (89.03%) - LeetHub

--- 0343-integer-break/0343-integer-break.java
+++ 0343-integer-break/0343-integer-break.java
@@ -2,11 +2,9 @@ class Solution {
     public int integerBreak(int n) {
         if(n<=3) return n-1;
         int result = 1;
-        while(n>=5){
-            result*=3;
-            n-=3;
-        }
-        return result*n;
+        if(n%3==0) return (int)Math.pow(3,n/3);
+        else if(n%3==1) return 4*(int)Math.pow(3,n/3 -1);
+        else return 2*(int)Math.pow(3,n/3);
         
     }
 }
