758ee9380e4f3d48411dc0968debc46d5ddc1af5
Furkan Doğmuş <69652349+furkandogmus@users.noreply.github.com>
2023-09-23 01:58:42 +0300

    Time: 1 ms (80.89%), Space: 40.2 MB (73.75%) - LeetHub

--- 0290-word-pattern/0290-word-pattern.java
+++ 0290-word-pattern/0290-word-pattern.java
@@ -1,8 +1,10 @@
 class Solution {
     public boolean wordPattern(String pattern, String s) {
+        
         Map<Character,String> hm = new HashMap<>();
         Map<String,Character> hm1 = new HashMap<>();
 		String[] sArr = s.split(" ");
+        
 		if(sArr.length!=pattern.length()) return false;
         
         for(int i=0;i<sArr.length;i++) {
@@ -10,7 +12,6 @@ class Solution {
 			else hm.put(pattern.charAt(i), sArr[i]);
             if(hm1.containsKey(sArr[i]) && pattern.charAt(i)!=hm1.get(sArr[i])) return false;
 			else hm1.put(sArr[i], pattern.charAt(i));
-            
 		}
 
 		return true;
