e96d101fc73a86b65e289002446d9c70f6d8c3ea
Furkan Doğmuş <69652349+furkandogmus@users.noreply.github.com>
2023-09-23 20:20:43 +0300

    Time: 44 ms (50.05%), Space: 43.3 MB (80.54%) - LeetHub

---
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -0,0 +1,35 @@
+class Solution {
+    public boolean canPartition(int[] nums) {
+        int sum = 0;
+        for(int i=0;i<nums.length;i++){
+            sum += nums[i];
+        }
+        if(sum%2!=0) return false;
+        sum /=2;
+        int n = nums.length;
+        boolean[] prev = new boolean[sum+1];
+  
+ 
+        for (int i = 1; i <= sum; i++){
+            prev[i] = false;
+        }
+    
+ 
+      
+        for (int i = 1; i <= n; i++) {
+        boolean[] curr = new boolean[sum + 1];
+            for (int j = 1; j <= sum; j++) {
+                if(j==nums[i-1]) curr[j] = true;
+                if (j < nums[i - 1])
+                    curr[j] = prev[j];
+                if (j > nums[i - 1])
+                    curr[j] = prev[j] || prev[j - nums[i - 1]];
+            }
+        prev = curr;
+    }
+ 
+    return prev[sum];
+        
+        
+    }
+}
