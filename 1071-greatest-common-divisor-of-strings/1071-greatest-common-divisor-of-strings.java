52a948c3f0e56fb95841e563702e25fd63def215
Furkan Doğmuş <69652349+furkandogmus@users.noreply.github.com>
2023-09-24 15:23:29 +0300

    Time: 35 ms (5.08%), Space: 44 MB (8.36%) - LeetHub

---
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.java
@@ -0,0 +1,30 @@
+class Solution {
+    public String gcdOfStrings(String str1, String str2) {
+        if(str1.charAt(0)!=str2.charAt(0)) return "";
+        StringBuilder res = new StringBuilder();
+        String result = "";
+
+        if(str1.length()<str2.length()){
+            String temp = str1;
+            str1= str2;
+            str2 = temp;
+        }
+        for(int i=0;i<str2.length();i++){
+            res.append(str2.charAt(i));
+            StringBuilder control = new StringBuilder(res.toString());
+            while(control.length()<str1.length()){
+                control.append(res.toString());
+            }
+            StringBuilder control2 = new StringBuilder(res.toString());
+            while(control2.length()<str2.length()){
+                control2.append(res.toString());
+            }
+            if(control.toString().equals(str1) && control2.toString().equals(str2)){
+                result = res.toString();
+            }
+
+
+        }
+        return result;
+    }
+}
