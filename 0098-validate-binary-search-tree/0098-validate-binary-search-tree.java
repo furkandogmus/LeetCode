2a65197e84245230e62966e8acc2a8ce6edb92cc
Furkan Doğmuş <69652349+furkandogmus@users.noreply.github.com>
2023-09-24 13:27:20 +0300

    Time: 0 ms (100.00%), Space: 44.4 MB (5.91%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
@@ -4,13 +4,9 @@ class Solution {
     }
     
     public boolean dfs(TreeNode root, Integer min, Integer max) {
-        if (root == null) {
-            return true;
-        }
+        if (root == null)  return true;
         
-        if ((min != null && root.val <= min) || (max != null && root.val >= max)) {
-            return false;
-        }
+        if ((min != null && root.val <= min) || (max != null && root.val >= max)) return false;
         
         return dfs(root.left, min, root.val) && dfs(root.right, root.val, max);
     }
