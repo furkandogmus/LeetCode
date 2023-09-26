a2a577e56f75a7f4d8ee0244268bf587ff820419
Furkan Doğmuş <69652349+furkandogmus@users.noreply.github.com>
2023-09-27 02:11:03 +0300

    Time: 0 ms (100.00%), Space: 39.1 MB (61.93%) - LeetHub

--- 0754-reach-a-number/0754-reach-a-number.java
+++ 0754-reach-a-number/0754-reach-a-number.java
@@ -1,17 +1,17 @@
 class Solution {
-   public int reachNumber(int target) {
-        target = Math.abs(target);
-        long n = (long) Math.ceil((-1.0 + Math.sqrt(1 + 8.0 * target)) / 2);
-        long sum = n * (n + 1) / 2;
-        if (sum == target) {
-            return (int) n;
-        }
-        long res = sum - target;
-        if ((res & 1) == 0) {
-            return (int) n;
-        } else {
-            return (int) (n + ((n & 1) == 1 ? 2 : 1));
-        }
-}
+    public int reachNumber(int target) {
+        if (target < 0) target = -target; 
+        int x = (int) Math.ceil((-1 + Math.sqrt(1 + 8L * target)) / 2);
+        long sum = (long) x * (x + 1) / 2;
+
+        if (sum == target) return x;
+
+        int res = (int) (sum - target);
+
+        if (res % 2 == 0) return x;
+        if (x % 2 == 0) return x + 1;
+        return x + 2;
+    }
+
 }
 
